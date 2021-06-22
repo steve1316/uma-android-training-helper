01ad8bf1ab2ba5672c16189a55250b5af358b86d
Steve Tu <stevetu11@yahoo.com>
2021-06-21 20:01:42 -0700

    Uploaded StatusData.kt

---
+++ app/src/main/java/com/steve1316/uma_android_training_helper/data/StatusData.kt
@@ -0,0 +1,16 @@
+package com.steve1316.uma_android_training_helper.data
+
+class StatusData {
+	companion object {
+		val status = mapOf(
+			"Bad Practice" to "Whoops! Watch your step! Increased training fail rate.",
+			"Charming" to "It's fun exercising together! Increased bond gain with your training partners.",
+			"Good Practice" to "Be prepared even in practice. Decreased training fail rate.",
+			"Insomnia" to "Tends to stay up late at night. May lose energy due to lack of sleep.",
+			"Lazy Habit" to "Eh, who cares about training? Sometimes just skips training.",
+			"Overweight" to "Ate too much and got a little fat. Cannot train speed.",
+			"Rising Star" to "Outstanding! The academy is proud of you! Journalists and the Principal will appreciate you more.",
+			"Sharp" to "Such a fast learner! All skills are discounted.",
+		)
+	}
+}
